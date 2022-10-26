5e357517e61339279a4b84009fe4929708ed4e56
Oroch <oliveirarocha_pedro@hotmail.com>
2022-10-25 22:03:18 -0300

    api laravel

--- ecommerce/routes/api.php
+++ ecommerce/routes/api.php
@@ -17,3 +17,26 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::prefix('v1')->group(static function(){ //prefixo para todos os endpoints
+
+    Route::get('/vendedores', [App\Http\Controllers\vendedoresApiController::class, 'index']); //get para obter informações, chama o método index da api controller.
+    Route::post('/vendedores', [App\Http\Controllers\vendedoresApiController::class, 'store']);
+    Route::delete('/vendedores/{id}', [App\Http\Controllers\vendedoresApiController::class, 'destroy']);
+    Route::put('/vendedores/{id}', [App\Http\Controllers\vendedoresApiController::class, 'update']);
+
+    Route::get('/clientes', [App\Http\Controllers\clientesApiController::class, 'index']); //get para obter informações, chama o método index da api controller.
+    Route::post('/clientes', [App\Http\Controllers\clientesApiController::class, 'store']);
+    Route::delete('/clientes/{id}', [App\Http\Controllers\clientesApiController::class, 'destroy']);
+    Route::put('/clientes/{id}', [App\Http\Controllers\clientesApiController::class, 'update']);
+
+    Route::get('/produtos', [App\Http\Controllers\produtoApiController::class, 'index']); //get para obter informações, chama o método index da api controller.
+    Route::post('/produtos', [App\Http\Controllers\produtoApiController::class, 'store']);
+    Route::delete('/produtos/{id}', [App\Http\Controllers\produtoApiController::class, 'destroy']);
+    Route::put('/produtos/{id}', [App\Http\Controllers\produtoApiController::class, 'update']);
+
+
+
+
+
+});
--- ecommerce/routes/web.php
+++ ecommerce/routes/web.php
@@ -22,3 +22,26 @@ Route::get('/dashboard', function () {
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 require __DIR__.'/auth.php';
+
+Route::prefix('v1')->group(static function(){ //prefixo para todos os endpoints
+
+    Route::get('/vendedores', [App\Http\Controllers\vendedoresApiController::class, 'index']); //get para obter informações, chama o método index da api controller.
+    Route::post('/vendedores', [App\Http\Controllers\vendedoresApiController::class, 'store']);
+    Route::delete('/vendedores/{id}', [App\Http\Controllers\vendedoresApiController::class, 'destroy']);
+    Route::put('/vendedores/{id}', [App\Http\Controllers\vendedoresApiController::class, 'update']);
+
+    Route::get('/clientes', [App\Http\Controllers\clientesApiController::class, 'index']); //get para obter informações, chama o método index da api controller.
+    Route::post('/clientes', [App\Http\Controllers\clientesApiController::class, 'store']);
+    Route::delete('/clientes/{id}', [App\Http\Controllers\clientesApiController::class, 'destroy']);
+    Route::put('/clientes/{id}', [App\Http\Controllers\clientesApiController::class, 'update']);
+
+    Route::get('/produtos', [App\Http\Controllers\produtoApiController::class, 'index']); //get para obter informações, chama o método index da api controller.
+    Route::post('/produtos', [App\Http\Controllers\produtoApiController::class, 'store']);
+    Route::delete('/produtos/{id}', [App\Http\Controllers\produtoApiController::class, 'destroy']);
+    Route::put('/produtos/{id}', [App\Http\Controllers\produtoApiController::class, 'update']);
+
+
+
+
+
+});
